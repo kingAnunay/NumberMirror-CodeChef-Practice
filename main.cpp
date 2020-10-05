769fe9becd98a93cbeb7301ffeea6f02a2514d08
KingAnunay <anunaynmit@gmail.com>
2020-10-05 20:33:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+  // NUMBER MIRROR
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n = 0;
+    cin>>n;
+    if(n>=0&&n<=100000){
+        cout<<n;
+    }
+	return 0;
+}
